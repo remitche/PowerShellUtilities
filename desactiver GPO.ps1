c3824318f29f21ef03a7f59238dc0b0070e046f9
remitche <remitchegrd@gmail.com>
2021-10-12 11:45:21 +0200

    Commit initiale, upload d'uns cript de changement de GPO
    Ajout d'un script qui désactive les politiques GPO de windows pour virer les merdes du types candy crush etc

---
+++ desactiver GPO.ps1
@@ -0,0 +1,16 @@
+﻿#Supression de la politique "GPO" pour enelever le crap du type, instagram, candycrush etc installé par défault dans Windows
+
+#Test de la présence de l'entrée dans le registre
+$path = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent';
+$key = "DisableWindowsConsumerFeatures "; #la clé en question
+$value = 0; #mettre à zéro pour désactiver
+
+$recherche = Test-Path $Path
+
+if( $recherche)
+{
+   New-ItemProperty -Path $path -Name $key -Value $value } #on remet ça à zéro
+else
+{
+    echo "Aucune présence de la politique GPO dans le registre :)";
+}
